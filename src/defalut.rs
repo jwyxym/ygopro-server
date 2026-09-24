c7a5bc857c7f0a7a46d9946d5654ca93c70fa748
jwyxym <jwyxym@126.com>
2026-09-24 11:44:24 +0800

    update 0.1.1

--- src/defalut.rs
+++ src/defalut.rs
@@ -14,14 +14,14 @@ pub use ygopro_data::{
 };
 pub static SCRIPT_BUFFER: Mutex<[u8; 0x100000]> = Mutex::new([0u8; 0x100000]);
 
-pub fn get_log_message(pduel: isize) -> Cow<'static, str> {
+pub fn get_log_message(pduel: isize) -> String {
 	let mut buffer: [u8; 1024] = [0u8; 1024];
 	unsafe {
 		ygopro_core_wrapper::get_log_message(pduel, buffer.as_mut_ptr());
 	}
 	let c_message: &CStr = unsafe { CStr::from_ptr(buffer.as_ptr() as *const c_char) };
 	let msg: Cow<'_, str> = c_message.to_string_lossy();
-	msg
+	msg.to_string()
 }
 
 pub extern "C" fn core_message_handler(_pduel: isize, _message_type: u32) -> u32 {
